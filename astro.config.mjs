fba236ca1d6200b4fb5c8dcd63bfd392ea8151fb
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-03-08 00:59:38 +0300

    try ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
+import { defineConfig } from "astro/config";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelServerless(),
   build: {
     inlineStylesheets: "never",
   },
+  devToolbar: { enabled: false },
   integrations: [
     // partytown(),
-    mdx()]
-  ,
+    mdx(),
+  ],
   vite: {
     css: {
       devSourcemap: true,
